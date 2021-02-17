1d3a86553fb5dfb12b11546f17009e7a4f5bc119
Srikanth Rachala <srikanth.rachala@gmail.com>
2021-02-16 21:53:03 -0800

    Initial Commit

---
+++ src/test/java/com/springcoding/springboot/mongodb/SpringMongodbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcoding.springboot.mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongodbDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
